23678897941ed5c453eec905768bd4fc410c70e2
Clothilde Drouet clothildedrouet@gmail.com <clothildedrouet@gmail.com>
2023-12-05 15:56:47 +0100

    indexview et map

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,5 +1,11 @@
 class ParksController < ApplicationController
   def index
     @parks = Park.all
+    @markers = @parks.geocoded.map do |park|
+      {
+        lat: park.latitude,
+        lng: park.longitude
+      }
+    end
   end
 end
