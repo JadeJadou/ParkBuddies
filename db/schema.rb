73eb93aeb4ae713da0adf444f4b33a3b78a9331f
Frédérique Laguerre <laguerre.frederique@gmail.com>
2023-12-06 14:11:20 +0100

    destroy all favorite

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2023_12_06_115941) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -105,6 +104,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_06_115941) do
     t.integer "kids_average_age"
     t.integer "kids_count"
     t.string "avatar"
+    t.text "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
