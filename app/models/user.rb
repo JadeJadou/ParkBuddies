6e8cccd575111744ebdd45986bf44ed8825a720d
Jade Grimm <grimmjade@live.fr>
2023-12-11 14:26:08 +0100

    AVAAAAAAAAAAAAAAAAAATAAAAAAAAAAAAAAAAAARRRRR DONE

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,25 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_one_attached :avatar
+
+  AVATAR_CHOICES = ["AV_chicken_green.png","AV_chicken_yellow.png","AV_chicken_purple.png","AV_chicken_black_green.png", "AV_chicken_black_coral.png",
+
+  "AV_fish_yellow.png","AV_fish_purple.png","AV_fish_black_green.png","AV_fish_black_coral.png",
+
+  "AV_frog_yellow.png","AV_frog_black_coral.png","AV_frog_black_green.png","AV_frog_purple.png","AV_frog_green.png",
+
+  "AV_kangaroo_black_coral.png", "AV_kangaroo_black_green.png", "AV_kangaroo_green.png", "AV_kangaroo_purple.png", "AV_kangaroo_yellow.png",
+
+  "AV_pinguin_black_green.png","AV_pinguin_black_coral.png","AV_pinguin_green.png","AV_pinguin_purple.png","AV_pinguin_yellow.png",
+
+  "AV_sheep_black_coral.png","AV_sheep_black_green.png","AV_sheep_green.png","AV_sheep_purple.png","AV_sheep_yellow.png",
+
+  "AV_sloth_purple.png","AV_sloth_yellow.png","AV_sloth_black_green.png","AV_sloth_black_coral.png",
+
+  "AV_toucan_green.png","AV_toucan_purple.png","AV_toucan_yellow.png","AV_toucan_black_green.png","AV_toucan_black_coral.png",
+
+  "AV_turkey_black_coral.png","AV_turkey_black_green.png","AV_turkey_green.png","AV_turkey_purple.png","AV_turkey_yellow.png",
+].sample(4)
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
