433a146f3418693e4be99ec0a1d639e854892406
Frédérique Laguerre <laguerre.frederique@gmail.com>
2023-12-12 15:10:39 +0100

    les notifications FONCTIONNE

--- app/models/private_message.rb
+++ app/models/private_message.rb
@@ -7,8 +7,12 @@ class PrivateMessage < ApplicationRecord
     user.id == a_user.id
   end
 
-  after_create_commit -> {
-    puts "test"
-     broadcast_append_to( "notification", partial: "private_messages/private_message_notif", locals: { private_message: self }, target: "notification_#{ self.private_chatroom.user_2.id }")
+  # after_create_commit -> {
+  #    broadcast_append_to( "notification", partial: "private_messages/private_message_notif", locals: { private_message: self }, target: "notification_#{ self.private_message.user == self.private_message.private_chatroom.user_1 ? self.private_message.private_chatroom.user_2 : self.private_message.private_chatroom.user_1}")
+  #   }
+    after_create_commit -> {
+      puts "test"
+      recipient_user = self.private_chatroom.user_1 == self.user ? self.private_chatroom.user_2 : self.private_chatroom.user_1
+      broadcast_append_to("notification", partial: "private_messages/private_message_notif", locals: { private_message: self }, target: "notification_#{recipient_user.id}")
     }
 end
