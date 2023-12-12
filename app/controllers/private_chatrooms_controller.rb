82976674ed42d87d90f87b98fe68009cf07d0f60
qianwang <wangqianlf@live.com>
2023-12-12 12:58:28 +0100

    private chatroom est dans index des chatrooms

--- app/controllers/private_chatrooms_controller.rb
+++ app/controllers/private_chatrooms_controller.rb
@@ -1,18 +1,18 @@
 class PrivateChatroomsController < ApplicationController
-
-  def index
-    @private_message = PrivateMessage.new
-    @private_chatroom = current_user.private_chatrooms
-    if params[:query].present?
-      @chatrooms = @chatrooms.where("name ILIKE ?", "%#{params[:query]}%")
-    end
-  end
+  # def index
+  #   @private_message = PrivateMessage.new
+  #   @private_chatroom = current_user.private_chatrooms
+  #   if params[:query].present?
+  #     @chatrooms = @chatrooms.where("name ILIKE ?", "%#{params[:query]}%")
+  #   end
+  # end
 
   def show
     @buddy = User.find(params[:buddy_id])
     @private_chatroom = PrivateChatroom.find_or_create_by(user_1: current_user, user_2: @buddy) do |chatroom|
       chatroom.user_1 = current_user
       chatroom.user_2 = @buddy
+      chatroom.name = "Discussion privée avec & #{@buddy.nickname}"
     end
     @private_message = PrivateMessage.new
     @private_messages = @private_chatroom.private_messages
